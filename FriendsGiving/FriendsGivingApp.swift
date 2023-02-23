a0a664469fdc29c32e71fc9b48791098363bc84c
Philip Keller <philipjoelkeller@gmail.com>
2023-02-22 22:29:25 -0800

    Initial Commit

---
+++ FriendsGiving/FriendsGivingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsGivingApp.swift
+//  FriendsGiving
+//
+//  Created by Philip Keller on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct FriendsGivingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
